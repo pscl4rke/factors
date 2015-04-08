e641e27515fdf62426503ba32beef69f4d44bc3f
P. S. Clarke <factors@pscl4rke.net>
2015-04-08 18:44:50 +0100

    Skeleton function that only handles primes.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+
+
+fn factors_of(n: usize) -> Vec<usize> {
+    vec![1, n]
+}
+
+
+#[test]
+fn test_factoring_of_prime_numbers() {
+    assert_eq!(factors_of(19), vec![1,19]);
+}
+
+
 fn main() {
     panic!("The CLI has not been implemented yet");
 }
